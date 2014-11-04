8194f9bf9b17634251ce3d5c3f3cadbf9d18ae45
Allison Thackston <allison.thackston@nasa.gov>
2014-11-04 17:05:39 -0600

    updated image_cb_detector to lock threads, removed command line arguments

--- image_cb_detector/src/calibration_detector.py
+++ image_cb_detector/src/calibration_detector.py
@@ -2,6 +2,7 @@
 import rospy
 import roslib
 import actionlib
+import threading
 
 roslib.load_manifest("image_cb_detector")
 from camera_calibrator.calibration_plate import *
@@ -11,19 +12,33 @@ from sensor_msgs.msg import Image, CameraInfo
 
 class CalibrationDetectorServer:
     def __init__(self, name):
+        self._lock = threading.Lock()
         self._action_name = name
+        self._board = None
+        
+        self._imageSub = rospy.Subscriber("image_raw", Image, self.img_callback)
+        rospy.loginfo("[%s] Subscribing to: %s" % (rospy.get_name(), rospy.remap_name("image_raw")))
+        self._featurePub = rospy.Publisher("features", CalibrationPattern)
+        self._calImagePub = rospy.Publisher("calibration_image", Image)
+        rospy.loginfo("[%s] Publishing features to: %s" % (rospy.get_name(), rospy.remap_name("features")))
+        rospy.loginfo("[%s] Publishing calibration_image to: %s" % (rospy.get_name(), rospy.remap_name("calibration_image")))
+        
         self._server = actionlib.SimpleActionServer(self._action_name, ConfigAction, None, False)
         self._server.register_goal_callback(self.goal_callback)
         self._server.register_preempt_callback(self.preempt_callback)
-        self._imageSub = rospy.Subscriber("image_raw", Image, self.img_callback)
-        self._featurePub = rospy.Publisher("features", CalibrationPattern)
-        self._calImagePub = rospy.Publisher("calibration_image", Image)
-        self._board = None
+
         self._server.start()
 
     def goal_callback(self):
+        self._lock.acquire()
         goal = self._server.accept_new_goal()
         rospy.loginfo("[%s] Starting calibration plate detector" % rospy.get_name())
+        rospy.loginfo("[%s] num_x: %s" %(rospy.get_name(), str(goal.num_x)))
+        rospy.loginfo("[%s] num_y: %s" %(rospy.get_name(), str(goal.num_y)))
+        rospy.loginfo("[%s] spacing_x: %s" %(rospy.get_name(), str(goal.spacing_x)))
+        rospy.loginfo("[%s] spacing_y: %s" % (rospy.get_name(), str(goal.spacing_y)))
+        rospy.loginfo("[%s] pattern: %s" % (rospy.get_name(), str(goal.pattern)))
+        rospy.loginfo("[%s] flags: %s" % (rospy.get_name(), str(goal.flags)))
         # create the calibrtion plate
         if goal.pattern == ConfigGoal.CHECKERBOARD:
             self._board = Chessboard(self._action_name, goal.num_x, goal.num_y, goal.spacing_x, goal.spacing_y, goal.flags)
@@ -33,19 +48,22 @@ class CalibrationDetectorServer:
             self._board = ACircles(self._action_name, goal.num_x, goal.num_y, goal.spacing_x, goal.spacing_y, goal.flags)
         else:
             self._server.set_aborted()
-            self._board = None
-            
+            #self._board = None
+        
+        self._lock.release()
         if self._server.is_preempt_requested():
-            self._board = None
             self._server.set_preempted()
         
     def preempt_callback(self):
-        self._board = None
+        self._lock.acquire()
+        #self._board = None
         self._server.set_preempted()
+        self._lock.release()
 
     def img_callback(self, img):
         rospy.logdebug("[%s] Got new image message" % rospy.get_name())
-        if(self._featurePub and self._board and self._server.is_active()):
+        if(self._server.is_active()):
+            self._lock.acquire()
             ok, corners, mono, (x_scale, y_scale) = self._board.detect(img)
             msg = self._board.mk_pattern_msg(ok, corners)
             msg.header = img.header
@@ -53,6 +71,7 @@ class CalibrationDetectorServer:
             mono_msg = self._board.mk_image_msg(mono, corners, ok)
             mono_msg.header = img.header
             self._calImagePub.publish(mono_msg)
+            self._lock.release()
 
 if __name__=='__main__':
     rospy.init_node("image_calibration_detector")
@@ -64,31 +83,6 @@ if __name__=='__main__':
     pattern = rospy.get_param("~pattern", ConfigGoal.CHECKERBOARD)
     flags = rospy.get_param("~flags", 0)
     
-    from optparse import OptionParser
-    parser = OptionParser("%prog", description=None)
-    parser.add_option("-a", "--autostart", dest="autostart", type="string", default=autostart,help="Autostart image_calibration_detector")
-    parser.add_option("-c", "--cols", dest= "num_cols", type="int", default=num_cols, help="Number of cols in calibration pattern")
-    parser.add_option("-r", "--rows", dest= "num_rows", type="int", default=num_rows, help="Number of rows in calibration pattern")
-    parser.add_option("-d", "--dim", dest = "dim", type="float", default=dim, help="The length in meters between calibration points")
-    parser.add_option("-p", "--pattern", dest="pattern", type="int", default=pattern, help="The type of calibration pattern (ENUM: 0: CHECKERBOARD, 1: CIRCLES, 2: ACIRCLES")
-    parser.add_option("-f", "--flags", dest="flags", type="int", default=flags, help="Flags to send to the calibrator")
-    
-    options, args = parser.parse_args()
-    
-    autostart = options.autostart.lower()=='true'
-    num_cols = options.num_cols
-    num_rows = options.num_rows
-    dim = options.dim
-    pattern = options.pattern
-    flags = options.flags
-    
-    rospy.loginfo("[%s][autostart] %s" % (rospy.get_name(), str(autostart)))
-    rospy.loginfo("[%s][num_cols] %s" %(rospy.get_name(), str(num_cols)))
-    rospy.loginfo("[%s][num_rows] %s" %(rospy.get_name(), str(num_rows)))
-    rospy.loginfo("[%s][dim] %s" % (rospy.get_name(), str(dim)))
-    rospy.loginfo("[%s][pattern] %s" % (rospy.get_name(), str(pattern)))
-    rospy.loginfo("[%s][flags] %s" % (rospy.get_name(), str(flags)))
-            
     CalibrationDetectorServer(rospy.get_name())
     
     if(autostart):
